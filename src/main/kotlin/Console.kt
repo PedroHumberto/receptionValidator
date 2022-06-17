e9b4977d8fe58e986f2f51efd58b5bfb662bd8ed
Pedro Humberto <pedrohlucape@gmail.com>
2022-06-17 18:03:15 -0300

    Classe Console, Classe Portaria, readInt e readString, fechado construtor da classe Console, companion object

--- src/main/kotlin/Console.kt
+++ src/main/kotlin/Console.kt
@@ -1,2 +1,40 @@
-class Console {
+class Console private constructor(){
+
+    companion object{
+        fun readInt(msg: String): Int{
+            var retorno: Int? = null
+            do{
+                print(msg)
+                val info = readLine()
+
+                if(info != null && info != ""){
+                    retorno = info.toIntOrNull()
+
+                    if (retorno == null || retorno <= 0){
+                        println("Valor Invalido")
+                    }
+                }else{
+                    println("Valor Invalido")
+                }
+            } while (retorno == null || retorno <= 0)
+
+            return retorno
+        }
+
+        fun readString(msg: String): String{
+            var retorno: String? = null
+            do {
+                print(msg)
+                val info = readLine()
+                if (info != null && info != ""){
+                    retorno = info.lowercase()
+                }else{
+                    println("Valor Invalido")
+                }
+            }while (retorno == null)
+            return retorno
+        }
+    }
+
+
 }
